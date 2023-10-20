2cad0d7ca0fcc687ca7df1d8fd63cb0c4f42f757
aliiosama <aliosama3060@gmail.com>
2023-10-19 17:10:35 -0700

    01

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,21 +16,3 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 #endif
-
-
-_putchar.c 
-
-#include <unistd.h>
-
-/**
-   * _putchar - writes the character c to stdout
-    * @c: The character to print
-     *
-      * Return: On success 1.
-       * On error, -1 is returned, and errno is set appropriately.
-        */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
-
