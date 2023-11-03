f814c46038a5703e7f7d37fb7f084e7a387b50c7
aliiosama <aliosama3060@gmail.com>
2023-11-02 17:54:37 -0700

    01

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,10 @@
 */
 void *malloc_checked(unsigned int b)
 {
-   void *i;
+	int *m = malloc(b);
 
+	if (m == 0)
+		exit(98);
 
-   i = malloc(b);
-   if (i == NULL)
-       exit(98);
-   return (i);
-}
-
+	return (m);
+}	
