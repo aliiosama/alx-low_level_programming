76d844e9e1798eaa6cecbcf82a7a2435c22edfa0
alii osama <aliosama3060@gmail.com>
2024-01-15 17:37:26 -0800

    02

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P https://github.com/sadatmisr/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+export LD_PRELOAD=/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
